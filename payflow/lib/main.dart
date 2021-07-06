fd789261bfabe0b85d43ff90d3463dec29e14561
schein00 <schein.jefer@gmail.com>
2021-07-05 21:17:07 -0300

    configurando o firebase

---
+++ payflow/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(PayFlow());
+}
+
+
+class PayFlow extends StatelessWidget {
+  const PayFlow({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
+      home: LoginPage(),
+    );
+  }
+}
+
+
+
