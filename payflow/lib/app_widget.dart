687f1300dbbe04d1bf61cce0be8475db294fc6a9
schein00 <schein.jefer@gmail.com>
2021-07-11 12:03:02 -0300

    use camera to read barcode

--- payflow/lib/app_widget.dart
+++ payflow/lib/app_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_bole_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
@@ -7,7 +9,12 @@ import 'modules/barcode_scanner/barcode_scaner_page.dart';
 import 'modules/login/login_page.dart';
 
 class PayFlow extends StatelessWidget {
-  const PayFlow({Key? key}) : super(key: key);
+  // const PayFlow({Key? key}) : super(key: key);
+
+  PayFlow() {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +29,8 @@ class PayFlow extends StatelessWidget {
         '/home': (context) => HomePage(),
         '/login': (context) => LoginPage(),
         '/splash': (context) => SplashPage(),
-        '/barcode_scanner': (context) => BarcodeScannerPage()
+        '/barcode_scanner': (context) => BarcodeScannerPage(),
+        '/insert_boleto': (context) => InsertBoletoPage(),
       },
     );
   }
